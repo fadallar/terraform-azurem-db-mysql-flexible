c0221ebd7e6e3394100feb68c01df2c48e4539f7
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-16 09:29:46 +0100

    Adding missing firewall configuration

--- network.tf
+++ network.tf
@@ -15,3 +15,12 @@
 #    }
 #  }
 #}
+resource "azurerm_mysql_flexible_server_firewall_rule" "firewall_rules" {
+  for_each = var.enable_public_access ? var.allowed_cidrs : {}
+
+  name                = each.key
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.mysql_flexible_server.name
+  start_ip_address    = cidrhost(each.value, 0)
+  end_ip_address      = cidrhost(each.value, -1)
+}
