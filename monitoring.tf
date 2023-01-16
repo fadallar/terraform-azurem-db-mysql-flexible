46ea00bfa7983dbd6b92f260d09f8c0eaea06b0b
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-16 13:24:05 +0100

    Fixing diagnostics settings

--- monitoring.tf
+++ monitoring.tf
@@ -2,12 +2,12 @@
 // Please check issue https://github.com/hashicorp/terraform-provider-azurerm/issues/17172
 
 data "azurerm_monitor_diagnostic_categories" "diagcategories" {
-  resource_id = azurerm_key_vault.keyvault.id
+  resource_id = azurerm_mysql_flexible_server.mysql_flexible_server.id
 }
 
 resource "azurerm_monitor_diagnostic_setting" "diagnostics" {
   name               = "default-Diagnostics-Logs"
-  target_resource_id = azurerm_key_vault.keyvault.id
+  target_resource_id = azurerm_mysql_flexible_server.mysql_flexible_server.id
   log_analytics_workspace_id = var.diag_log_analytics_workspace_id
 
   dynamic "metric" {
