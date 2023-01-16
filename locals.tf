aff0077eb7bedfdba25ecf56711540568f927191
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-16 09:07:27 +0100

    Fixing locals typo

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
-//administrator_login    = azurerm_mysql_flexible_server.mysql_flexible_server.administrator_login
-// Set password to custom password if provide otherwise auto-generate password
+locals {
 administrator_password = coalesce(var.custom_mysql_administrator_password, random_password.mysql_administrator_password.result)
 
 tier_map = {
@@ -11,3 +10,4 @@ tier_map = {
 default_mysql_options = {
     require_secure_transport = var.ssl_enforced ? "ON" : "OFF"
   }
+}
